6e34de819ffda9c8fea4c2512e4c22638f1d1e3a
Asfar17 <117092696+Asfar17@users.noreply.github.com>
2026-03-15 02:42:20 +0530

    Add geocode API handler for fetching geocode data

---
+++ api/geocode.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+
+  try {
+
+    const backend = await fetch(
+      "http://136.114.173.194:5000/api/geocode",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req.body)
+      }
+    );
+
+    const data = await backend.json();
+
+    res.status(200).json(data);
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: "Backend request failed",
+      message: err.message
+    });
+
+  }
+
+}
