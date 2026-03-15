ee24ca73d9e424c2dc233f51b81980326e74e3f7
Asfar17 <117092696+Asfar17@users.noreply.github.com>
2026-03-15 15:10:28 +0530

    Update API endpoint in status handler

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const n = req.query.n || 120;
 
-  const response = await fetch(`http://136.114.173.194:5000/status?n=${n}`);
+  const response = await fetch(`http://104.198.225.12/status?n=${n}`);
   const data = await response.json();
 
   res.status(200).json(data);
