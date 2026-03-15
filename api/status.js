65187c658267e80ef5f3d6218ebc538e73d8258a
Asfar17 <117092696+Asfar17@users.noreply.github.com>
2026-03-15 21:41:25 +0530

    Update status.js

--- api/status.js
+++ api/status.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   try {
 
     const backend = await fetch(
-      `http://104.198.225.12:5000/status?n=${n}`
+      `http://136.115.91.50:5000/status?n=${n}`
     );
 
     const data = await backend.json();
